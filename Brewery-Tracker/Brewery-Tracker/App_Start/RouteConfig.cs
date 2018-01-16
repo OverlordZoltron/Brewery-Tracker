ea038af7c9116252d17a7d1097fd8298e2b1b582
ian <ian_arsenault@protonmail.com>
2018-01-15 23:39:29 -0500

    add: page style and map routes

---
+++ Brewery-Tracker/Brewery-Tracker/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Brewery_Tracker
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Contact", "Contact", new { controller = "Home", action = "Contact" });
+
+            routes.MapRoute("About", "About", new { controller = "Home", action = "About" });
+
+            routes.MapRoute("Signup", "Signup", new { controller = "Home", action = "Signup" });
+
+            routes.MapRoute("Login", "Login", new { controller = "Home", action = "Login" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Brewery-Tracker/Brewery-Tracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Brewery_Tracker.MvcApplication" Language="C#" %>
